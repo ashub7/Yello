bb130f78fd529481ab7c45fa916607e07a56da30
Adm <ashubhandari77@gmail.com>
2024-11-25 19:10:49 +0530

    Initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'app/YelloApp.dart';
 
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  //FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const YelloApp());
 }
